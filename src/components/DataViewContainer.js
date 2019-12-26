56fee05a0376406c4013e575bd525fcc38e86d8d
xuanli328 <xuan.3.li@uconn.edu>
2019-12-26 16:14:54 -0500

    Add more components

---
+++ src/components/DataViewContainer.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import _ from 'lodash';
+import ShotChart from './ShotChart';
+import CountSlider from './CountSlider';
+import { Radio, Row, Col, Switch } from 'antd';
+
+const RadioGroup = Radio.Group;
+
+class DataViewContainer extends Component {
+    state = {
+        minCount: 2,
+        chartType: 'hexbin',
+        displayTooltip: true,
+    }
+
+    onCountSliderChange = (count) => {
+        this.setState({ minCount: Number(count) || 2 });
+    }
+
+    onChartTypeChange = (e) => {
+        this.setState({ chartType: e.target.value });
+    }
+
+    onTooltipChange = (displayTooltip) => {
+        this.setState({ displayTooltip });
+    }
+
+    render() {
+        return (
+            <div className="data-view">
+                <ShotChart
+                    playerId={this.props.playerId}
+                    minCount={this.state.minCount}
+                    chartType={this.state.chartType}
+                    displayTooltip={this.state.displayTooltip}
+                />
+                <div className="filters">
+                    {this.state.chartType === 'hexbin' ?
+                        <CountSlider value={this.state.minCount}
+                                     onCountSliderChange={_.debounce(this.onCountSliderChange, 500)}/> : null}
+                    <br/>
+                    <Row className="chart-type-radio">
+                        <Col span={10} >
+                            <RadioGroup onChange={this.onChartTypeChange} value={this.state.chartType}>
+                                <Radio value="hexbin">Hexbin</Radio>
+                                <Radio value="scatter">Scatter</Radio>
+                            </RadioGroup>
+                        </Col>
+                        <Col span={6}>
+                            Tooltip:{' '}
+                            <Switch
+                                checkedChildren="On"
+                                unCheckedChildren="Off"
+                                onChange={this.onTooltipChange}
+                                defaultChecked />
+                        </Col>
+                    </Row>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DataViewContainer;
